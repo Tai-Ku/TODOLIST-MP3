59ea447b02ca01c4e46c7a550ef96d18fc70975d
Tan Tai <taicucom@gmail.com>
2023-05-08 14:23:23 +0700

    volume && spiner

--- src/api/home.js
+++ src/api/home.js
@@ -1,15 +1,14 @@
 import axios from "../axios";
 
-
-export const getHome =() => new Promise(async(resolve, reject) => {
+export const getHome = () =>
+  new Promise(async (resolve, reject) => {
     try {
-        const response = await axios({
-            url:'/home',
-            method: 'GET',
-        })
-        resolve(response)
+      const response = await axios({
+        url: "/home",
+        method: "GET",
+      });
+      resolve(response);
     } catch (error) {
-        reject(error);
+      reject(error);
     }
-})
-
+  });
